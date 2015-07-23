efbe605d27a03a1d91dda6e4ae0a35c600c63efa
Nathaniel J. Smith <njs@pobox.com>
2015-07-22 18:59:35 -0700

    Add 2.6 to the setup.py list of supported versions

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
